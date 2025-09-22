2ff19f85bddc58c27b19ae3bc911864e5acbfb51
AngelVT <angelovelaz32@gmail.com>
2025-09-21 21:11:24 -0600

    added connection to db with ssl as required by supabase

--- server/src/config/database.config.js
+++ server/src/config/database.config.js
@@ -6,7 +6,13 @@ export const pool =  new Sequelize(IM_DB_DATABASE, IM_DB_USER, IM_DB_PASSWORD, {
     port: IM_DB_PORT,
     dialect: IM_DB_DIALECT,
     logging: false,
-    timezone: IM_DB_TIMEZONE
+    timezone: IM_DB_TIMEZONE,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 export async function initSchemas() {
