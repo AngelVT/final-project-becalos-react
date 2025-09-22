6224ba4ab2af04e8fc2a408f848ddfa6341b6707
AngelVT <angelovelaz32@gmail.com>
2025-09-21 21:04:21 -0600

    added PORT as an option instead of IM_APP_PORT

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 import app from "./app.js";
 import { IM_API_PORT } from "./config/environment.config.js";
 
-const server = app.listen(IM_API_PORT, () => {
-    console.log("Listening on port: ", IM_API_PORT);
+const { PORT } = process.env;
+
+const server = app.listen(IM_API_PORT || PORT, () => {
+    console.log("Listening on port: ", IM_API_PORT || PORT);
 });
